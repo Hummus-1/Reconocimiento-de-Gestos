510b172c78873849bad6ca48f3fe832a38e8f7c1
Jaime <72421644+alu0101329093@users.noreply.github.com>
2022-01-21 20:34:07 +0000

    Arreglado comentarios

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import math
 
-# Función que obtene el ángulo del defecto de convexidad
+# Funcion que obtiene el angulo del defecto de convexidad
 def angle(s,e,f):
     v1 = [s[0]-f[0],s[1]-f[1]]
     v2 = [e[0]-f[0],e[1]-f[1]]
@@ -15,7 +15,7 @@ def angle(s,e,f):
         ang += 2*np.pi
     return ang*180/np.pi
 
-# Función que obtiene el contorno de la mano y el hull
+# Funcion que obtiene el contorno de la mano y el hull
 def getcnthull(image):
     contours, hierarchy = cv2.findContours(image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
     contours = max(contours, key=lambda x: cv2.contourArea(x))
@@ -28,7 +28,7 @@ def getdefects(contours):
     defects = cv2.convexityDefects(contours, hull) # Almacena los defectos de convexidad
     return defects
 
-# Función que cuenta los dedos
+# Funcion que cuenta los dedos
 def countFingers(fingerConvexityDefects, handRect):
     numberOfFingers = len(fingerConvexityDefects)
     if numberOfFingers > 0:
@@ -38,7 +38,7 @@ def countFingers(fingerConvexityDefects, handRect):
     else:
         return 0
 
-# Función que detecta los gestos
+# Funcion que detecta los gestos
 def detectHandGesture(numberOfFingers, fingerConvexityDefects, handRect):
     if handRect[2] > 25 and handRect[3] > 25:
         if numberOfFingers == 0:
@@ -88,7 +88,7 @@ while True:
         if defects is not None:
             count = 0
             fingerConvexityDefects = list()
-            # Por cada defecto de convexidad hallamos sus puntos start, end, far, su ángulo y su profundidad
+            # Por cada defecto de convexidad hallamos sus puntos start, end, far, su angulo y su profundidad
             for i in range(len(defects)):
                 s,e,f,d = defects[i,0]
                 start = tuple(contours[s][0])
@@ -105,7 +105,7 @@ while True:
             cv2.rectangle(frame[pt1[1]:pt2[1],pt1[0]:pt2[0],:],pt1a,pt2a,(0,0,255),3)
             count = countFingers(fingerConvexityDefects, rect)
             gesture = detectHandGesture(count,fingerConvexityDefects, rect)
-            # Motrar que detectamos (nº de dedos y gesto)
+            # Motrar que detectamos (numero de dedos y gesto)
             cv2.putText(frame, str(count), (0, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv2.LINE_AA)
             cv2.putText(frame, gesture, (0, 100), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv2.LINE_AA)
         # Mostramos los distintos elementos es pantalla
